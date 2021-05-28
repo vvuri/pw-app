d6a308985edfdb0cbb50a13cf6fe2bf6d3bedfe9
Yuri Vinogradov <vvuri1978@gmail.com>
2021-05-28 18:44:21 +0300

    Empty Angular app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PostComponent } from './post/post.component';
+import { PostFormComponent } from './post-form/post-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostComponent,
+    PostFormComponent
   ],
   imports: [
     BrowserModule
